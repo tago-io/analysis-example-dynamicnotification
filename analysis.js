7f4de5bb40d4039a434372159dc8d8ea1d7bc2ea
Vitor Lima <vitor@ferreiradelima.com>
2016-08-22 19:36:49 -0300

    Added Hello World to console

--- analysis.js
+++ analysis.js
@@ -2,7 +2,12 @@
 const Analysis = require('tago/analysis');
 
 function myanalysis(context, scope) {
+    context.log("Hello World");
+    
+    //Example, passing context to console
     context.log('my context:', context);
+
+    //Example, passing scope to console
     context.log('my scope:', scope);
 }
 
