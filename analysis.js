99f539493d607ca5a702e49d3cb8976ce51103c5
rcanoff <rcanoff@gmail.com>
2018-06-06 16:27:04 -0300

    Updated the comments

--- analysis.js
+++ analysis.js
@@ -1,7 +1,7 @@
 const Analysis = require('tago/analysis');
 
 // This is a hello world example to the Tago Analysis
-// The function myAnalysis will be ran when you execute your analysis
+// The function myAnalysis will run when you execute your analysis
 function myAnalysis(context, scope) {
     // This will log "Hello World" at the Tago Analysis console
     context.log("Hello World");
