bb719875096c5331663c0a2f6d577e9de87695a5
rcanoff <rcanoff@gmail.com>
2018-06-11 11:32:00 -0300

    Changed the comments

--- analysis.js
+++ analysis.js
@@ -1,16 +1,23 @@
+/* 
+ * Analysis Example
+ * Hello World
+ * 
+ * Learn how to send messages to the console located on the Tago analysis screen.
+ * You can use this principle to show any information during and after development.
+ */
+
 const Analysis = require('tago/analysis');
 
-// This is a hello world example to the Tago Analysis
 // The function myAnalysis will run when you execute your analysis
 function myAnalysis(context, scope) {
-    // This will log "Hello World" at the Tago Analysis console
-    context.log("Hello World");
+  // This will log "Hello World" at the Tago Analysis console
+  context.log("Hello World");
     
-    // This will log the context to the Tago Analysis console
-    context.log('Context:', context);
+  // This will log the context to the Tago Analysis console
+  context.log('Context:', context);
 
-    // This will log the scope to the Tago Analysis console
-    context.log('my scope:', scope);
+  // This will log the scope to the Tago Analysis console
+  context.log('my scope:', scope);
 }
 
 // The analysis token in only necessary to run the analysis outside Tago
