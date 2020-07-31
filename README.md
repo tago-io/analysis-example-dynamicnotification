8171a5f3b776f9bfad50861dba739cdc1522c55b
Felipe Lima <felipe@ferreiradelima.com>
2020-07-30 21:28:22 -0300

    update example to use new sdk

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Simple example using Tago Analysis
+## TagoIO - Analysis Example
 
 You can just upload the `analysis.js` file.
 
 or
 
-You can run locally, you should have last node version and install all dependencies running `npm install` on your terminal in this project folder, 
+You can run locally, you should have last node version and install all dependencies running `npm install` on your terminal in this project folder,
 after that, you can run the project just calling analysis, `node analysis.js`;
--- analysis.js
+++ analysis.js
@@ -1,24 +1,28 @@
-/* 
- * Analysis Example
+/*
+ * TagoIO - Analysis Example
  * Hello World
- * 
- * Learn how to send messages to the console located on the Tago analysis screen.
+ *
+ * Check out the SDK documentation on: https://js.sdk.tago.io
+ *
+ * Learn how to send messages to the console located on the TagoIO analysis screen.
  * You can use this principle to show any information during and after development.
- */
+*/
 
-const Analysis = require('tago/analysis');
+const { Analysis } = require("@tago-io/sdk");
 
 // The function myAnalysis will run when you execute your analysis
 function myAnalysis(context, scope) {
-  // This will log "Hello World" at the Tago Analysis console
+  // This will log "Hello World" at the TagoIO Analysis console
   context.log("Hello World");
-    
-  // This will log the context to the Tago Analysis console
-  context.log('Context:', context);
 
-  // This will log the scope to the Tago Analysis console
-  context.log('my scope:', scope);
+  // This will log the context to the TagoIO Analysis console
+  context.log("Context:", context);
+
+  // This will log the scope to the TagoIO Analysis console
+  context.log("my scope:", scope);
 }
 
-// The analysis token in only necessary to run the analysis outside Tago
-module.exports = new Analysis(myAnalysis, 'MY-ANALYSIS-TOKEN-HERE');
+module.exports = new Analysis(myAnalysis);
+
+// To run analysis on your machine (external)
+// module.exports = new Analysis(myAnalysis, { token: "YOUR-TOKEN" });
